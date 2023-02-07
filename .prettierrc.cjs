0db8763a7b5b3b7d52b04e8aa724ed0661ad5623
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-02-07 14:34:24 -0500

    Add prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  pluginSearchDirs: false,
+  printWidth: 100,
+};
