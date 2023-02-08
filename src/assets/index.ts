d7ead0df7b21ed1dca054fc308faa590816ed472
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-02-08 13:35:26 -0500

    Add assets

---
+++ src/assets/index.ts
@@ -0,0 +1,10 @@
+import Skills from "@/assets/skills";
+import Icons from "@/assets/icons";
+
+import JLetter from "@/assets/JLetter.astro";
+import Logo from "@/assets/Logo.astro";
+import MailIllustration from "@/assets/MailIllustration.astro";
+
+const OtherAssets = [JLetter, Logo, MailIllustration];
+
+export { Skills, Icons, OtherAssets };
---
+++ src/assets/skills/index.ts
@@ -0,0 +1,41 @@
+import chakraui from "@/assets/skills/chakraui.png";
+import css from "@/assets/skills/css.png";
+import cypress from "@/assets/skills/cypress.png";
+import docker from "@/assets/skills/docker.png";
+import figma from "@/assets/skills/figma.png";
+import git from "@/assets/skills/git.png";
+import graphql from "@/assets/skills/graphql.png";
+import html from "@/assets/skills/html.png";
+import javascript from "@/assets/skills/javascript.png";
+import mongodb from "@/assets/skills/mongodb.png";
+import nextjs from "@/assets/skills/nextjs.png";
+import node from "@/assets/skills/node.png";
+import postgresql from "@/assets/skills/postgresql.png";
+import react from "@/assets/skills/react.png";
+import restapi from "@/assets/skills/restapi.png";
+import sass from "@/assets/skills/sass.png";
+import strapi from "@/assets/skills/strapi.png";
+import typescript from "@/assets/skills/typescript.png";
+
+const skills = [
+  chakraui,
+  css,
+  cypress,
+  docker,
+  figma,
+  git,
+  graphql,
+  html,
+  javascript,
+  mongodb,
+  nextjs,
+  node,
+  postgresql,
+  react,
+  restapi,
+  sass,
+  strapi,
+  typescript,
+];
+
+export default skills;
