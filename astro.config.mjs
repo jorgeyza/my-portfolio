5f41a93e79edf6ceaea118d45cd596f0058431f3
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-02-10 18:28:11 -0500

    Add Radix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,21 +5,19 @@ import { fileURLToPath } from "url";
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-
+import react from "@astrojs/react";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), react()],
   vite: {
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-  },
+        "@": path.resolve(__dirname, "./src")
+      }
+    }
+  }
 });
