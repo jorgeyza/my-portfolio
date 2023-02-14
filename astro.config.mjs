eab3cdfe5b7470351abdbc5684fcedf062a7ed8c
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-02-14 12:51:23 -0500

    Code formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,27 @@
 import path from "path";
 import { fileURLToPath } from "url";
 
-// https://astro.build/config
 import { defineConfig } from "astro/config";
+
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import react from "@astrojs/react";
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), react()],
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    react(),
+  ],
   vite: {
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "./src")
-      }
-    }
-  }
+        "~": path.resolve(__dirname, "./src"),
+      },
+    },
+  },
 });
