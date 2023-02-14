fa0a325beaaf5581c8addc78bfffc35f80c73ec6
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-02-14 12:54:19 -0500

    Update animation duration

--- src/components/MobileNavigation/Hamburger.tsx
+++ src/components/MobileNavigation/Hamburger.tsx
@@ -8,22 +8,22 @@ const Hamburger = ({ isOpen }: Props) => {
   return (
     <>
       <span
-        className={`absolute left-0 top-0 block h-1 w-full rotate-0 rounded-sm bg-slate-50 opacity-100 duration-300 ease-in-out${
+        className={`absolute left-0 top-0 block h-1 w-full rotate-0 rounded-sm bg-slate-50 opacity-100 duration-500 ease-in-out${
           isOpen ? " top-2 left-1/2 w-0" : ""
         }`}
       />
       <span
-        className={`absolute left-0 top-2 block h-1 w-full rotate-0 rounded-sm bg-slate-50 opacity-100 duration-300 ease-in-out${
+        className={`absolute left-0 top-2 block h-1 w-full rotate-0 rounded-sm bg-slate-50 opacity-100 duration-500 ease-in-out${
           isOpen ? " rotate-45" : ""
         }`}
       />
       <span
-        className={`absolute left-0 top-2 block h-1 w-full rotate-0 rounded-sm bg-slate-50 opacity-100 duration-300 ease-in-out${
+        className={`absolute left-0 top-2 block h-1 w-full rotate-0 rounded-sm bg-slate-50 opacity-100 duration-500 ease-in-out${
           isOpen ? " -rotate-45" : ""
         }`}
       />
       <span
-        className={`absolute left-0 top-4 block h-1 w-full rotate-0 rounded-sm bg-slate-50 opacity-100 duration-300 ease-in-out${
+        className={`absolute left-0 top-4 block h-1 w-full rotate-0 rounded-sm bg-slate-50 opacity-100 duration-500 ease-in-out${
           isOpen ? " top-2 left-1/2 w-0" : ""
         }`}
       />
