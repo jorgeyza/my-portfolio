61bb86d3fc32da8fca749cca226869739ac00260
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-02-14 12:58:01 -0500

    Update skills exported array

--- src/assets/skills/index.ts
+++ src/assets/skills/index.ts
@@ -1,89 +1,41 @@
-import chakraui from "@/assets/skills/chakraui.png";
-import css from "@/assets/skills/css.png";
-import cypress from "@/assets/skills/cypress.png";
-import docker from "@/assets/skills/docker.png";
-import figma from "@/assets/skills/figma.png";
-import git from "@/assets/skills/git.png";
-import graphql from "@/assets/skills/graphql.png";
-import html from "@/assets/skills/html.png";
-import javascript from "@/assets/skills/javascript.png";
-import mongodb from "@/assets/skills/mongodb.png";
-import nextjs from "@/assets/skills/nextjs.png";
-import node from "@/assets/skills/node.png";
-import postgresql from "@/assets/skills/postgresql.png";
-import react from "@/assets/skills/react.png";
-import restapi from "@/assets/skills/restapi.png";
-import sass from "@/assets/skills/sass.png";
-import strapi from "@/assets/skills/strapi.png";
-import typescript from "@/assets/skills/typescript.png";
+import chakraui from "~/assets/skills/chakraui.png";
+import css from "~/assets/skills/css.png";
+import cypress from "~/assets/skills/cypress.png";
+import docker from "~/assets/skills/docker.png";
+import figma from "~/assets/skills/figma.png";
+import git from "~/assets/skills/git.png";
+import graphql from "~/assets/skills/graphql.png";
+import html from "~/assets/skills/html.png";
+import javascript from "~/assets/skills/javascript.png";
+import mongodb from "~/assets/skills/mongodb.png";
+import nextjs from "~/assets/skills/nextjs.png";
+import node from "~/assets/skills/node.png";
+import postgresql from "~/assets/skills/postgresql.png";
+import react from "~/assets/skills/react.png";
+import restapi from "~/assets/skills/restapi.png";
+import sass from "~/assets/skills/sass.png";
+import strapi from "~/assets/skills/strapi.png";
+import typescript from "~/assets/skills/typescript.png";
 
 const skills = [
-  { src: html.src, alt: "html", width: html.width, height: html.height, label: "HTML" },
-  { src: css.src, alt: "css", width: css.width, height: css.height, label: "CSS" },
-  {
-    src: javascript.src,
-    alt: "javascript",
-    width: javascript.width,
-    height: javascript.height,
-    label: "JavaScript",
-  },
-  {
-    src: typescript.src,
-    alt: "typescript",
-    width: typescript.width,
-    height: typescript.height,
-    label: "TypeScript",
-  },
-  { src: sass.src, alt: "sass", width: sass.width, height: sass.height, label: "Sass" },
-  {
-    src: chakraui.src,
-    alt: "chakraui",
-    width: chakraui.width,
-    height: chakraui.height,
-    label: "ChakraUI",
-  },
-  {
-    src: cypress.src,
-    alt: "cypress",
-    width: cypress.width,
-    height: cypress.height,
-    label: "Cypress",
-  },
-  { src: react.src, alt: "react", width: react.width, height: react.height, label: "React" },
-  { src: nextjs.src, alt: "nextjs", width: nextjs.width, height: nextjs.height, label: "NextJS" },
-  { src: node.src, alt: "node", width: node.width, height: node.height, label: "NodeJS" },
-  {
-    src: graphql.src,
-    alt: "graphql",
-    width: graphql.width,
-    height: graphql.height,
-    label: "GraphQL",
-  },
-  {
-    src: restapi.src,
-    alt: "restapi",
-    width: restapi.width,
-    height: restapi.height,
-    label: "REST API",
-  },
-  { src: git.src, alt: "git", width: git.width, height: git.height, label: "Git" },
-  { src: docker.src, alt: "docker", width: docker.width, height: docker.height, label: "Docker" },
-  {
-    src: mongodb.src,
-    alt: "mongodb",
-    width: mongodb.width,
-    height: mongodb.height,
-    label: "MongoDB",
-  },
-  {
-    src: postgresql.src,
-    alt: "postgresql",
-    width: postgresql.width,
-    height: postgresql.height,
-    label: "PostgreSQL",
-  },
-  { src: strapi.src, alt: "strapi", width: strapi.width, height: strapi.height, label: "Strapi" },
-  { src: figma.src, alt: "figma", width: figma.width, height: figma.height, label: "Figma" },
+  { src: html, alt: "html", label: "HTML" },
+  { src: css, alt: "css", label: "CSS" },
+  { src: javascript, alt: "javascript", label: "JavaScript" },
+  { src: typescript, alt: "typescript", label: "TypeScript" },
+  { src: sass, alt: "sass", label: "Sass" },
+  { src: chakraui, alt: "chakraui", label: "ChakraUI" },
+  { src: cypress, alt: "cypress", label: "Cypress" },
+  { src: react, alt: "react", label: "React" },
+  { src: nextjs, alt: "nextjs", label: "NextJS" },
+  { src: node, alt: "node", label: "NodeJS" },
+  { src: graphql, alt: "graphql", label: "GraphQL" },
+  { src: restapi, alt: "restapi", label: "REST API" },
+  { src: git, alt: "git", label: "Git" },
+  { src: docker, alt: "docker", label: "Docker" },
+  { src: mongodb, alt: "mongodb", label: "MongoDB" },
+  { src: postgresql, alt: "postgresql", label: "PostgreSQL" },
+  { src: strapi, alt: "strapi", label: "Strapi" },
+  { src: figma, alt: "figma", label: "Figma" },
 ];
 
 export default skills;
