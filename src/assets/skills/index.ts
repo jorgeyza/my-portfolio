bb622840d82e14944645a199671270cd27611349
Jorge Eyzaguirre <jorgeyza@gmail.com>
2023-03-03 16:44:22 -0500

    Add skills

--- src/assets/skills/index.ts
+++ src/assets/skills/index.ts
@@ -1,5 +1,6 @@
 import astro from "~/assets/skills/astro.png";
 import chakraui from "~/assets/skills/chakraui.png";
+import materialui from "~/assets/skills/materialui.png";
 import css from "~/assets/skills/css.png";
 import cypress from "~/assets/skills/cypress.png";
 import docker from "~/assets/skills/docker.png";
@@ -11,6 +12,7 @@ import javascript from "~/assets/skills/javascript.png";
 import mongodb from "~/assets/skills/mongodb.png";
 import nextjs from "~/assets/skills/nextjs.png";
 import node from "~/assets/skills/node.png";
+import prisma from "~/assets/skills/prisma.png";
 import postgresql from "~/assets/skills/postgresql.png";
 import react from "~/assets/skills/react.png";
 import restapi from "~/assets/skills/restapi.png";
@@ -25,13 +27,15 @@ const skills = [
   { src: javascript, alt: "javascript", label: "JavaScript" },
   { src: typescript, alt: "typescript", label: "TypeScript" },
   { src: sass, alt: "sass", label: "Sass" },
-  { src: chakraui, alt: "chakraui", label: "ChakraUI" },
+  { src: chakraui, alt: "chakra ui", label: "Chakra UI" },
+  { src: materialui, alt: "material ui", label: "MUI" },
   { src: tailwind, alt: "tailwind", label: "Tailwind" },
   { src: cypress, alt: "cypress", label: "Cypress" },
   { src: react, alt: "react", label: "React" },
   { src: nextjs, alt: "nextjs", label: "NextJS" },
   { src: astro, alt: "astro", label: "Astro" },
   { src: node, alt: "node", label: "NodeJS" },
+  { src: prisma, alt: "prisma", label: "Prisma" },
   { src: graphql, alt: "graphql", label: "GraphQL" },
   { src: restapi, alt: "restapi", label: "REST API" },
   { src: git, alt: "git", label: "Git" },
