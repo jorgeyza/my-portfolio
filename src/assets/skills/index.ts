77f04bf1f952f76e20f81d3febf4500f0c9c661f
Jorge Eyzaguirre <jorgeyza@gmail.com>
2024-08-08 16:51:42 -0500

    Update skills

--- src/assets/skills/index.ts
+++ src/assets/skills/index.ts
@@ -1,8 +1,9 @@
 import astro from "~/assets/skills/astro.png";
-import chakraui from "~/assets/skills/chakraui.png";
 import css from "~/assets/skills/css.png";
 import cypress from "~/assets/skills/cypress.png";
 import docker from "~/assets/skills/docker.png";
+import express from "~/assets/skills/express.png";
+import fastify from "~/assets/skills/fastify.png";
 import figma from "~/assets/skills/figma.png";
 import git from "~/assets/skills/git.png";
 import graphql from "~/assets/skills/graphql.png";
@@ -10,7 +11,6 @@ import html from "~/assets/skills/html.png";
 import javascript from "~/assets/skills/javascript.png";
 import materialui from "~/assets/skills/materialui.png";
 import mongodb from "~/assets/skills/mongodb.png";
-import mysql from "~/assets/skills/mysql.png";
 import nextjs from "~/assets/skills/nextjs.png";
 import node from "~/assets/skills/node.png";
 import odoo from "~/assets/skills/odoo.png";
@@ -19,7 +19,6 @@ import postgresql from "~/assets/skills/postgresql.png";
 import python from "~/assets/skills/python.png";
 import react from "~/assets/skills/react.png";
 import restapi from "~/assets/skills/restapi.png";
-import strapi from "~/assets/skills/strapi.png";
 import tailwind from "~/assets/skills/tailwind.png";
 import typescript from "~/assets/skills/typescript.png";
 
@@ -28,8 +27,7 @@ const skills = [
   { src: css, alt: "css", label: "CSS" },
   { src: javascript, alt: "javascript", label: "JavaScript" },
   { src: typescript, alt: "typescript", label: "TypeScript" },
-  { src: python, alt: "python", label: "python" },
-  { src: chakraui, alt: "chakra ui", label: "Chakra UI" },
+  { src: python, alt: "python", label: "Python" },
   { src: materialui, alt: "material ui", label: "MUI" },
   { src: tailwind, alt: "tailwind", label: "Tailwind" },
   { src: cypress, alt: "cypress", label: "Cypress" },
@@ -37,6 +35,8 @@ const skills = [
   { src: nextjs, alt: "nextjs", label: "Next.js" },
   { src: astro, alt: "astro", label: "Astro" },
   { src: node, alt: "node", label: "Node.js" },
+  { src: express, alt: "express", label: "Express" },
+  { src: fastify, alt: "fastify", label: "Fastify" },
   { src: prisma, alt: "prisma", label: "Prisma" },
   { src: graphql, alt: "graphql", label: "GraphQL" },
   { src: restapi, alt: "restapi", label: "REST API" },
@@ -44,8 +44,6 @@ const skills = [
   { src: docker, alt: "docker", label: "Docker" },
   { src: mongodb, alt: "mongodb", label: "MongoDB" },
   { src: postgresql, alt: "postgresql", label: "PostgreSQL" },
-  { src: mysql, alt: "mysql", label: "MySQL" },
-  { src: strapi, alt: "strapi", label: "Strapi" },
   { src: figma, alt: "figma", label: "Figma" },
   { src: odoo, alt: "odoo", label: "Odoo" },
 ];
